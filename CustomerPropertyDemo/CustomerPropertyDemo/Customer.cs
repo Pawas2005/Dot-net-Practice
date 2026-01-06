4269a66b6bacb1e9f4abe41dd43d4a18a3d69f67
Pawas Ranjan <pawasranjan5@gmail.com>
2026-01-06 19:59:40 +0530

    created a CustomerPropertyDemo using Structure.

---
+++ CustomerPropertyDemo/CustomerPropertyDemo/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerPropertyDemo
+{
+    class Customer
+    {
+        List<Orders> orderList;
+
+        public Customer()
+        {
+            orderList = new List<Orders>();
+
+        }
+        public int CustId { get; set; }
+        public string Name { get; set; }
+        public Address BillingAddr { get; set; }
+        public Address ShippingAddr { get; set; }
+        public List<Orders> MyOrders
+        {
+            get
+            {
+                return orderList;
+            }
+            protected set
+            {
+                orderList = value;
+            }
+        }
+    }
+}
---
+++ CustomerPropertyDemo/CustomerPropertyDemo/PrimeCustomers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerPropertyDemo
+{
+    class PrimeCustomers : Customer
+    {
+        public List<Orders> MyPrimeOrders //Write only property
+        {
+            set
+            {
+                MyOrders = value;
+            }
+        }
+    }
+}
