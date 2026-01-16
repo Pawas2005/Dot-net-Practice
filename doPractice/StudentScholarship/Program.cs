e464b4f76fb115fac8fa6d5bcc5b4bbc18ee779b
Pawas Ranjan <pawasranjan5@gmail.com>
2026-01-16 20:18:06 +0530

    Develop a StudentScholarship Program using delegate, List.

---
+++ doPractice/StudentScholarship/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace StudentScholarship
+{
+    public delegate bool IsEligibleforScholarship(Student std);
+    public class Program
+    {
+        public static bool ScholarshipEligibility(Student std)
+        {
+            if(std.Marks > 80 && std.SportsGrade == 'A')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string GetEligibleStudents(List<Student> studentsList, IsEligibleforScholarship isEligible)
+        {
+            List<string> eligibleNames = new List<string>();
+
+            foreach(var name in studentsList)
+            {
+                if (isEligible(name))
+                {
+                    eligibleNames.Add(name.Name);
+                }
+            }
+
+            return string.Join(", ", eligibleNames);
+        }
+
+        public static void Main()
+        {
+            List<Student> lstStudents = new List<Student>();
+
+            Student obj1 = new Student() { RollNo = 1, Name = "Raj", Marks = 75, SportsGrade = 'A' };    
+            Student obj2 = new Student() { RollNo = 2, Name = "Rahul", Marks = 82, SportsGrade = 'A' };
+            Student obj3 = new Student() { RollNo = 3, Name = "Kiran", Marks = 89, SportsGrade = 'B' };
+            Student obj4 = new Student() { RollNo = 4, Name = "Sunil", Marks = 86, SportsGrade = 'A' };
+
+            lstStudents.Add(obj1);    
+            lstStudents.Add(obj2);   
+            lstStudents.Add(obj3);   
+            lstStudents.Add(obj4);
+
+            //instance of delegate
+            IsEligibleforScholarship del = ScholarshipEligibility;
+
+            string result = GetEligibleStudents(lstStudents, del);
+
+            Console.WriteLine(result);           
+        }
+    }
+}
---
+++ doPractice/StudentScholarship/Student.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace StudentScholarship
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public int Marks { get; set; }
+        public char SportsGrade { get; set; }
+    }
+}
