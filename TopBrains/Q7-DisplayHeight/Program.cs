67f332cf9713454d41793b1fb4039a1e68691163
Pawas Ranjan <pawasranjan5@gmail.com>
2026-01-19 21:48:48 +0530

    Display-height Program

---
+++ TopBrains/Q7-DisplayHeight/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DisplayHeight
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter the height in CM : ");
+            int height = int.Parse(Console.ReadLine());
+
+            if(height < 150)
+            {
+                Console.WriteLine("Short");
+            }
+            else if(height >= 180)
+            {
+                Console.WriteLine("Tall");
+            }
+            else
+            {
+                Console.WriteLine("Average");
+            }
+        }
+    }
+}
