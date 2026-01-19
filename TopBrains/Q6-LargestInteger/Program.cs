d691775dbde7e14f87c6ecd2f301495c1934373f
Pawas Ranjan <pawasranjan5@gmail.com>
2026-01-19 20:43:43 +0530

    Develop a largest of three integers program using conditional logic.

---
+++ TopBrains/Q6-LargestInteger/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LargestInteger
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter the value of a : ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the value of b : ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the value of c : ");
+            int c = int.Parse(Console.ReadLine());
+
+            if(a > b && a > c)
+            {
+                Console.WriteLine($"\na : {a} is the largest.");
+            }
+            else if(b > a && b > c)
+            {
+                Console.WriteLine($"\nb : {b} is the largest.");
+            }
+            else
+            {
+                Console.WriteLine($"\nc : {c} is the largest.");
+            }
+        }
+    }
+}
