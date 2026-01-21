64e5f174f5f250e8087dc6c231f9d2a483e8e697
Pawas Ranjan <pawasranjan5@gmail.com>
2026-01-21 20:43:05 +0530

    Develop a File I/O program.

---
+++ TopBrains/Q14-FileIO/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FileInpOP
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string inputFile = "log.txt";
+            string outputFile = "error.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(inputFile);
+
+                using (StreamWriter writer = new StreamWriter(outputFile))
+                {
+                    foreach(string line in lines)
+                    {
+                        if (line.Contains("ERROR"))
+                        {
+                            writer.WriteLine(line);
+                        }
+                    }
+                }
+                Console.WriteLine("ERROR logs extracted successfully to error.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("log.txt file not found.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
