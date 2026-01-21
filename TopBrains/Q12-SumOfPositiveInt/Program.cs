48b5f80194da08482f5171e41a2fcf8fb41d72bf
Pawas Ranjan <pawasranjan5@gmail.com>
2026-01-21 15:14:18 +0530

    Develop a Sum Of Positive Integers Program.

---
+++ TopBrains/Q12-SumOfPositiveInt/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SumOfPositiveIntegers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter the value of n : ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] nums = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            int sum = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if(nums[i] > 0)
+                {
+                    sum += nums[i]; //only add +ve values
+                }
+                else if(nums[i] == 0) 
+                {
+                    break; //stop when 0 comes
+                }
+                else 
+                {
+                    continue; //ignore negatives
+                }
+            }
+
+            Console.WriteLine("Sum : " + sum);
+        }
+    }
+}
