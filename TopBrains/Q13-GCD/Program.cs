3b3151c08ff5e2911e9be2f7ddb2da8bf2261563
Pawas Ranjan <pawasranjan5@gmail.com>
2026-01-21 15:36:12 +0530

    Develop a Greatest Common Divisor (GCD) of two non-negative integers using recursion (Euclid's algorithm) Program.

---
+++ TopBrains/Q13-GCD/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GreatestCommonDivisor
+{
+    public class Program
+    {
+        //Simple approach
+        // public static int gcd(int a, int b)
+        // {
+        //     while(b != 0)
+        //     {
+        //         int temp = b;
+        //         b = a % b;
+        //         a = temp;
+        //     }
+        //     return a;
+        // }
+
+        //Recursive approach
+        public static int gcd(int a, int b)
+        {
+            if(b == 0)
+            {
+                return a;
+            }
+
+            return gcd(b, a % b);
+        }
+
+        public static void Main()
+        {
+            Console.Write("Enter the value of a : ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the value of b : ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"G.C.D of a: {a} & b: {b} = " + gcd(a, b));
+        }
+    }
+}
