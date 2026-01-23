dde47f5e901852967b1a4360a3f579bbf2e8fc43
Pawas Ranjan <pawasranjan5@gmail.com>
2026-01-23 22:43:05 +0530

    Develop a Simple Parsing Program.

---
+++ TopBrains/Q16-Parsing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Parsing
+{
+    public class Program
+    {
+        public static int sumValidInt(string[] tokens)
+        {
+            int sum = 0;
+            foreach(string token in tokens)
+            {
+                if(int.TryParse(token, out int value))
+                {
+                    sum += value;
+                }
+            }
+            return sum;
+        }
+
+        public static void Main()
+        {
+            string[] tokens = { "10", "abc", "20", "99999999999", "-5", "3.5", "40" };
+            int res = sumValidInt(tokens);
+
+            Console.WriteLine(res);   
+        }
+    }
+}
