9cfe85a10cbbbf29fff15815f533401b37e726d4
Pawas Ranjan <pawasranjan5@gmail.com>
2026-02-13 14:44:06 +0530

    Word Frequency Counter – Count occurrences ignoring case and punctuation.

---
+++ Strings/WordFreqCnter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace WordFrequencyCounter;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the Word: ");
+        string inp = Console.ReadLine();
+
+        inp = inp.ToLower();
+
+        inp = Regex.Replace(inp, @"[^\w\s]", "");   //FOR REPLACING PUNCTUATION
+        
+        string[] words = inp.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+        Dictionary<string, int> wordcnt = new Dictionary<string, int>();
+
+        foreach(string word in words)
+        {
+            if (wordcnt.ContainsKey(word))
+            {
+                wordcnt[word]++;
+            }
+            else
+            {
+                wordcnt[word] = 1;
+            }
+        }
+
+        Console.WriteLine("\nWords Frequencies: ");
+        foreach(var pair in wordcnt)
+        {
+            Console.WriteLine(pair.Key + " : " + pair.Value);
+        }
+    }
+}
