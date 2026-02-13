781a7f783cd9fee5d7720af94bccc8112e6f66b8
Pawas Ranjan <pawasranjan5@gmail.com>
2026-02-13 20:02:07 +0530

    Complete The Inventory Management Program.

--- EXAMPR/InventoryMgmtSystem/ProductUtility.cs
+++ EXAMPR/InventoryMgmtSystem/ProductUtility.cs
@@ -5,30 +5,28 @@ public class ProductUtility
 {
     private List<Product> ProductList = new List<Product>();
 
-    public double CalculateTotalValue(int Quantity, double PricePerUnit)
+    public double CalculateTotalValue(int Quantity, double Price)
     {
-        double total = Quantity * PricePerUnit;
-
-        return total;
+        return Quantity * Price;
     }
-
-    public void LastUpdatedDate(DateTime LastUpdated)
-    {
-        DateTime CurrDate = DateTime.Now;
-        
-        LastUpdated = CurrDate;
-    }
-
 
     public void AddProduct(Product product)
     {
+        product.TotalValue = CalculateTotalValue(product.Quantity, product.PricePerUnit);
+        product.LastUpdated = DateTime.Now;
         ProductList.Add(product);
         Console.WriteLine("Product Added Successfully.");
-        Console.WriteLine($"TOtal Stock Value: {CalculateTotalValue}");
+        Console.WriteLine($"TOtal Stock Value: {product.TotalValue}");
     }
 
     public void DisplayProducts()
     {
+        if(ProductList.Count == 0)
+        {
+            Console.WriteLine("No Products Available");
+            return;
+        }
+
         foreach(var p in ProductList)
         {
             Console.WriteLine($"Details: {p.ProductId} {p.ProductName} {p.Quantity} {p.TotalValue} {p.LastUpdated}");
@@ -42,7 +40,53 @@ public class ProductUtility
         if(result != null)
         {
             result.Quantity = qty;
-            double recalTotal = CalculateTotalValue(qty, result.PricePerUnit);
+            result.TotalValue = CalculateTotalValue(qty, result.PricePerUnit);
+            result.LastUpdated = DateTime.Now;
+
+            Console.WriteLine("Updated Successfully");
+        }
+        else
+        {
+            Console.WriteLine("Product Not Found");
+        }
+    }
+
+    public void DeleteProduct(string id)
+    {
+        var product = ProductList.FirstOrDefault(p => p.ProductId == id);
+
+        if(product != null)
+        {
+            ProductList.Remove(product);
+            Console.WriteLine("Product deleted successfully");
+        }
+        else
+        {
+            Console.WriteLine("Product Not Found");
+        }
+    }
+
+    public void SearchProduct(string id)
+    {
+        var product = ProductList.FirstOrDefault(p => p.ProductId == id);
+
+        if (product != null)
+        {
+            Console.WriteLine($"Found: {product.ProductId} {product.ProductName} {product.TotalValue}");
+        }
+        else
+        {
+            Console.WriteLine("Product Not Found");
+        }
+    }
+
+    public void SortByStockValue()
+    {
+        var sorted = ProductList.OrderBy(p => p.ProductId);
+
+        foreach(var p in sorted)
+        {
+            Console.WriteLine($"{p.ProductId} {p.ProductName} {p.TotalValue}");
         }
     }
 }
--- EXAMPR/InventoryMgmtSystem/Program.cs
+++ EXAMPR/InventoryMgmtSystem/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+namespace InventoryMgmtSystem;
+
+public class Program
+{
+    public static void Main()
+    {
+        ProductUtility utility = new ProductUtility();
+
+        while (true)
+        {
+            Console.WriteLine("\n1 Add Product");
+            Console.WriteLine("2 Display All Products");
+            Console.WriteLine("3 Update Quantity");
+            Console.WriteLine("4 Search Product");
+            Console.WriteLine("5 Delete Product");
+            Console.WriteLine("6 Sort By Stock Value");
+            Console.WriteLine("7 Exit");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    string input = Console.ReadLine();
+                    string[] data = input.Split(' ');
+
+                    Product product = new Product()
+                    {
+                        ProductId = data[0],
+                        ProductName = data[1],
+                        Quantity = int.Parse(data[2]),
+                        PricePerUnit = double.Parse(data[3])
+                    };
+
+                    utility.AddProduct(product);
+                    break;
+
+                case 2:
+                    utility.DisplayProducts();
+                    break;
+
+                case 3:
+                    string up = Console.ReadLine();
+                    string[] upd = up.Split(' ');
+
+                    utility.UpdateQuantity(upd[0], int.Parse(upd[1]));
+                    break;
+
+                case 4:
+                    string search = Console.ReadLine();
+
+                    utility.SearchProduct(search);
+                    break;
+
+                case 5:
+                    string del = Console.ReadLine();
+
+                    utility.DeleteProduct(del);
+                    break;
+
+
+                case 6:
+                    utility.SortByStockValue();
+                    break;
+
+                case 7:
+                    Console.WriteLine("Thank you");
+                    return;
+            }
+        }
+    }
+}
