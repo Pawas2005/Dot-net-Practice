febfa6a0f44e756866e3aa8f1e05de7010a2d868
Pawas Ranjan <pawasranjan5@gmail.com>
2026-02-14 11:13:58 +0530

    Develop a Non REpeated Char Program.

---
+++ EXAMPR/MovieStock/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MovieStock;
+
+public class Program
+{
+    public static List<Movie> MovieList = new List<Movie>();
+
+    public static void AddMovie(string MovieDetails)
+    {
+        
+    }
+}
---
+++ Strings/NonRepeatedChar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+// namespace  NonRepeatedCharacters;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter the String: ");
+        string str = Console.ReadLine();
+
+        var result = str.GroupBy(c => c)
+                        .Where(g => g.Count() == 1)
+                        .Select(g => g.Key)
+                        .FirstOrDefault();
+
+        if(result != '\0')
+        {
+            Console.WriteLine("First non-repeated Character: " + result);
+        }
+        else
+        {
+            Console.WriteLine("No non-repeated Character found.");
+        }
+    }
+}
