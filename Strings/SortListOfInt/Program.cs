1417e387e9c56d212a3a610b145043b485e53a5c
Pawas Ranjan <pawasranjan5@gmail.com>
2026-02-14 12:10:38 +0530

    Develop a Sort List Of Integers Program using Steam Functions.

---
+++ Strings/SortListOfInt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+namespace SortListOfIntegers;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the integers separated by space: ");
+        List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        var sortedInt = numbers.OrderBy(x => x);
+        
+        Console.WriteLine("Sorted List Of Integers are : ");
+        foreach(var item in sortedInt)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
