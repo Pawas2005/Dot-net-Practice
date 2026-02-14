7a67f52bbcce8d07a307e168659494f836512bbd
Pawas Ranjan <pawasranjan5@gmail.com>
2026-02-14 11:27:34 +0530

    Develop a First Repeated Char Program.

---
+++ Strings/FirstRepeatedChar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace FirstRepeatedChar;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter the String: ");
+        string str = Console.ReadLine();
+
+        var result = str.GroupBy(c => c)
+                        .Where(c => c.Count() > 1)
+                        .Select(c => c.Key)
+                        .FirstOrDefault();
+
+        if(result != '\0')
+        {
+            Console.WriteLine("First Repeated Character : " + result);
+        }
+        else
+        {
+            Console.WriteLine("No First Repeated Character Found.");
+        }
+    }
+}
