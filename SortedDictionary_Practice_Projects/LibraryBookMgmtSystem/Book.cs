95babb4dcf8b13beeca6b4f22e3600ab4a0dfaec
Pawas Ranjan <pawasranjan5@gmail.com>
2026-02-17 11:09:13 +0530

    Develop a Library Book Management System Program.

---
+++ SortedDictionary_Practice_Projects/LibraryBookMgmtSystem/Book.cs
@@ -0,0 +1,11 @@
+using System;
+namespace LibraryBookMgmtSystem;
+
+public class Book
+{
+    public string ISBN { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public bool IsAvailable { get; set; }
+}
