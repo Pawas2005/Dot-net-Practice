ae5f450c511290f48aebdc35f478ce36a2403952
Pawas Ranjan <pawasranjan5@gmail.com>
2026-02-24 22:36:35 +0530

    Develop a MVC_Core_WebApp Program.

---
+++ MVC_Core_WebApp1/MVC_Core_WebApp1/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Core_WebApp1.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Roll No is required.")]
+        public int RollNo { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(15,MinimumLength = 2,ErrorMessage = "Name must be between 2 and 15 characters.")]
+        public string Name { get; set; }
+
+        [Range(18,60,ErrorMessage = "Age must be between 18 and 60.")]
+        public int Age { get; set; }
+
+
+        public string Address { get; set; }
+    }
+}
---
+++ MVC_Core_WebApp1/MVC_Core_WebApp1/Models/StudentRepo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC_Core_WebApp1.Models
+{
+    public class StudentRepo : IRepo<Student>
+    {
+        public static List<Student> studList = null;
+
+        public StudentRepo()
+        {
+            if (studList == null)
+            {
+                //Collection Initializer
+                studList = new List<Student>()
+                {
+                    new Student() {RollNo = 101, Name = "Alok", Age = 22, Address = "Pune" },
+                    new Student() {RollNo = 102, Name = "Amit", Age = 24, Address = "Mumbai" }
+                };
+            }
+        }
+        public bool AddData(Student obj)
+        {
+            bool flag = false;
+            if(obj != null)
+            {
+                studList.Add(obj);
+                flag = true;
+            }
+            else
+            {
+                throw new NullReferenceException("Object is not Initialized");
+            }
+            return flag;
+        }
+
+        public bool DeleteData(int id)
+        {
+            bool flag = false;
+            Student sObj = studList.Find(s => s.RollNo == id);
+            if (sObj != null)
+            {
+                studList.Remove(sObj);
+                flag = true;
+            }
+            return flag;
+        }
+
+        public List<Student> ShowAllData()
+        {
+            return studList;
+        }
+
+        public Student ShowDetailsByID(int id)
+        {
+            Student sObj = studList.Find(s => s.RollNo == id);
+            return sObj;
+        }
+
+        public bool UpdateData(int id, Student obj)
+        {
+            bool flag = false;
+            Student sObj = studList.Find(s => s.RollNo == id);
+            if (sObj != null)
+            {
+                sObj.Name = obj.Name;
+                sObj.Age = obj.Age;
+                sObj.Address = obj.Address;
+                flag = true;
+            }
+            return flag;
+        }
+    }
+}
